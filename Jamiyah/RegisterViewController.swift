0d813d8c769156acb8e776fd04a1ce140971f010
Faisal Alkhotaifi <faisalalkhotaifi@Faisals-MacBook-Pro.local>
2017-05-08 00:09:35 -0700

    خلصنا تسجيل الدخول و انشاء حساب بس باقي نسيت كملة المرور

--- Jamiyah/RegisterViewController.swift
+++ Jamiyah/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
@@ -25,6 +26,44 @@ class RegisterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func register_buttonTobbed(_ sender: Any) {
+        let email = email_textField.text!.lowercased()
+        let final_email = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        let username = name_textField.text!
+        let password = password_textField.text!
+        let uuid = NSUUID().uuidString
+        
+        if (final_email.isEmpty || username.isEmpty || password.isEmpty){
+            alert_message(userMessage: "يجب اكمال جميع الحقول 😒")
+        }
+        else {
+            FIRAuth.auth()?.createUser(withEmail: final_email, password: password, completion: { (user, error) in
+                if (error != nil) {
+                    self.alert_message(userMessage: (error?.localizedDescription)!)
+                }
+                else {
+                    self.setUserInfo(user_uid: uuid, email: final_email, username: username)
+                    FIRAuth.auth()?.signIn(withEmail: final_email, password: password, completion: nil)
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainView")
+                    self.present(vc!, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    func alert_message (userMessage: String){
+        let alert = UIAlertController(title: "تنبيه", message: userMessage, preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "الغاء", style: .cancel, handler: nil)
+        alert.addAction(dismiss)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func setUserInfo (user_uid: String, email: String, username: String){
+        let ref: FIRDatabaseReference!
+        ref = FIRDatabase.database().reference().child("Users")
+        let user_list = ref.child("User UID\(user_uid)")
+        
+        user_list.child("E-mail address").setValue(email)
+        user_list.child("Username").setValue(username)
     }
     
 }
--- Jamiyah/SignInController.swift
+++ Jamiyah/SignInController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInController: UIViewController {
     @IBOutlet weak var email_textField: UITextField!
@@ -23,9 +24,37 @@ class SignInController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func forgotPassword_buttonTabbed(_ sender: Any) {
-    }
-    @IBAction func signIn_buttonTabbed(_ sender: Any) {
         
+    }
+    
+    
+    @IBAction func signIn_buttonTabbed(_ sender: Any) {
+        let email = email_textField.text!.lowercased()
+        let final_email = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = password_textField.text!
+
+        
+        if (final_email.isEmpty || password.isEmpty){
+            alert_message(userMessage: "يجب اكمال جميع الحقول 😒")
+        }
+        else {
+            FIRAuth.auth()?.signIn(withEmail: final_email, password: password, completion: { (user, error) in
+                if (error != nil) {
+                    self.alert_message(userMessage: (error?.localizedDescription)!)
+                }
+                else {
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainView")
+                    self.present(vc!, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    func alert_message (userMessage: String){
+        let alert = UIAlertController(title: "تنبيه", message: userMessage, preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "الغاء", style: .cancel, handler: nil)
+        alert.addAction(dismiss)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
